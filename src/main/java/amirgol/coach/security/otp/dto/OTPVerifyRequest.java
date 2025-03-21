905d79d954270567003f62a26481a2ea164084af
AmirGolmoradi <LeoQueenzer2.0@gmail.com>
2025-03-21 06:34:56 +0330

    feat: Define OTPVerifyRequest DTO for requesting OTP verification
    This commit introduces the `OTPVerifyRequest` Data Transfer Object (DTO). This class is used to encapsulate the data required to request the verification of a One-Time Password.
    
    It includes the following fields with validation constraints:
    - `email`: The email address associated with the OTP. It is marked as `@NotBlank` and `@Email` for validation.
    - `otp`: The One-Time Password to be verified. It is marked as `@NotBlank`.
    
    This class adheres to the Single Responsibility Principle by focusing solely on holding the data needed for an OTP verification request.

---
+++ src/main/java/amirgol/coach/security/otp/dto/OTPVerifyRequest.java
@@ -0,0 +1,21 @@
+package amirgol.coach.security.otp.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class OTPVerifyRequest {
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
+    private String email;
+
+    @NotBlank(message = "OTP is required")
+    private String otp;
+}
