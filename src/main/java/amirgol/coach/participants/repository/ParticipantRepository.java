fe42468c0f5e87e36c86f5cce80121d701f8bd38
AmirGolmoradi <LeoQueenzer2.0@gmail.com>
2025-03-28 15:28:25 +0330

    feat: Implement ParticipantRepository using Spring Data JPA
    This commit introduces the `ParticipantRepository` interface, which extends Spring Data JPA's `JpaRepository` for the `Participants` entity.
    
    It provides the following methods for data access:
    - Standard CRUD operations inherited from `JpaRepository`.
    - Custom methods for finding participants by email (`findByEmail`) and username (`findByUsername`).
    - Custom methods for checking the existence of participants by email (`existsByEmail`) and username (`existsByUsername`).
    - Custom methods for deleting participants by email (`deleteByEmail`) and username (`deleteByUsername`).

--- src/main/java/amirgol/coach/participants/repository/ParticipantRepository.java
+++ src/main/java/amirgol/coach/participants/repository/ParticipantRepository.java
@@ -2,9 +2,7 @@ package amirgol.coach.participants.repository;
 
 import amirgol.coach.participants.model.Participants;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface ParticipantRepository extends JpaRepository<Participants, Long> {
     Participants findByEmail(String email);
 
