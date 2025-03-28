b956615e06fef79757c0e074525785778b4222db
AmirGolmoradi <LeoQueenzer2.0@gmail.com>
2025-03-28 15:21:20 +0330

    feat: Configure Swagger (OpenAPI) for API documentation
    This commit sets up the Swagger (OpenAPI) configuration for the Coach API.
    
    - Defined an `OpenAPI` bean with basic API information: title ("Coach API"), version ("1.0"), and description ("API for participant management").
    - Configured a security scheme named "bearer-jwt" to represent JWT authentication using the Bearer token format.
    - Added a global security requirement to enforce the use of the "bearer-jwt" scheme for all API endpoints, allowing for secure testing and documentation in Swagger UI.
    
    This configuration enables the generation of interactive API documentation using Swagger UI, secured with JWT authentication.

--- src/main/java/amirgol/coach/config/SwaggerConfig.java
+++ src/main/java/amirgol/coach/config/SwaggerConfig.java
@@ -1,7 +1,10 @@
 package amirgol.coach.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,9 +12,18 @@ import org.springframework.context.annotation.Configuration;
 public class SwaggerConfig {
 
     @Bean
-    public OpenAPI nofiTechOpenAPI() {
-        return new OpenAPI().info(new Info().title("Coach API")
-                .version("1.0")
-                .description("API for Coach Applications"));
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Coach API")
+                        .version("1.0")
+                        .description("API for participant management"))
+                .components(new Components()
+                        .addSecuritySchemes("bearer-jwt",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")))
+                .addSecurityItem(new SecurityRequirement().addList("bearer-jwt"));
     }
 }
